5117e48cab7dbeea099878d6f157321055e45cb2
esraaehab333 <esraaehabb333@gmail.com>
2025-12-28 11:47:34 +0200

    Add printHelloGit to the HelloITI Class

--- HelloITI.java
+++ HelloITI.java
@@ -1,7 +1,10 @@
 class HelloWorld {
     public static void main(String[] args) {
         void printHelloITI() {
-      System.out.println("Hello, ITI!");
+            System.out.println("Hello, ITI!");
+        }
+        void printHelloGit(){
+           
         }
     }
 }
